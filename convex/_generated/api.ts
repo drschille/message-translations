b301f361c3b0614139fe4b2ee3217c7cc1f5c4c5
Daniel Robert Schille <13994573+drschille@users.noreply.github.com>
2026-03-22 18:58:16 +0100

    feat: Generate API and server utilities for Convex functions

--- convex/_generated/api.ts
+++
@@ -1,9 +0,0 @@
-import { anyApi, ApiFromModules } from "convex/server";
-import type * as sermons from "../sermons";
-
-/**
- * A utility for referencing Convex functions in your app's queries and mutations.
- */
-export const api: ApiFromModules<{
-  sermons: typeof sermons;
-}> = anyApi as any;
